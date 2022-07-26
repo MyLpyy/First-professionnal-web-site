c49e7d406f8532e84ef1ec30f326e0f307853cab
MyLpyy <thibrobi@gmail.com>
2022-07-26 14:04:48 +0200

    Try fix cors

--- back/app.js
+++ back/app.js
@@ -16,12 +16,6 @@ app.use(
 );
 app.use(cors());
 
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next()
-});
-
 app.use("/products", productRouter);
 app.use("/customers", customersRouter);
 app.use("/user_as_products", user_as_productsRouter);
